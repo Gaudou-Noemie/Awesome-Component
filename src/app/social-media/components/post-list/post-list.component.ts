cad5d7b4c359b9c4aae1b4fd770a4f2b6fe02050
Noémie Gaudou <gaudou.noemie@gmail.com>
2025-05-16 15:50:36 +0200

    Ecoute de l'envoie du commentaire

--- src/app/social-media/components/post-list/post-list.component.ts
+++ src/app/social-media/components/post-list/post-list.component.ts
@@ -1,3 +1,4 @@
+import { PostsService } from './../../services/posts.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map, Observable } from 'rxjs';
@@ -12,11 +13,17 @@ export class PostListComponent implements OnInit {
 
  posts$!: Observable<Post[]>;
 
-  constructor(private route: ActivatedRoute){}
+  constructor(private route: ActivatedRoute,
+              private postsService: PostsService) { }
+  
 
   ngOnInit(): void {
     this.posts$ = this.route.data.pipe(
       map(data => data['posts'])
     )
+  }
+
+  onPostCommented(postCommented: {comment: string, postId: number}){
+      this.postsService.addNewComment(postCommented)
   }
 }
--- src/app/social-media/services/posts.service.ts
+++ src/app/social-media/services/posts.service.ts
@@ -6,9 +6,14 @@ import { HttpClient} from '@angular/common/http'
 import { environment } from "src/environments/environment";
 @Injectable()
 export class PostsService {
+
     constructor(private http: HttpClient){}
     getPosts(): Observable<Post[]> {
         return this.http.get<Post[]>(`${environment.apiUrl}/posts`);
+    }
+
+    addNewComment(postCommented: { comment: string, postId: number}) {
+        console.log(postCommented);
     }
 }
 
