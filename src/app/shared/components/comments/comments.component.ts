e2a7abc7fe51980622d19e9a88598952fc3589b3
Noémie Gaudou <gaudou.noemie@gmail.com>
2025-05-16 15:09:56 +0200

    Ajout d'un controle sur le formulaire de commentaire.

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,20 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Comment } from 'src/app/core/models/comment.model';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
 })
-export class CommentsComponent implements OnInit{
-onLeaveComment() {
-throw new Error('Method not implemented.');
-}
- 
-  @Input() comments!: Comment[]
-  constructor() {}
+export class CommentsComponent implements OnInit {
+
+  @Input() comments!: Comment[];
+
+  commentCtrl!: FormControl;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    
+    this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)])
+  }
+
+  onLeaveComment() {
+    throw new Error('Method not implemented.');
   }
 }
